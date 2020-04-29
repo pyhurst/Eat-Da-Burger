2556cb7b55d449262cc15114bbc61773eb9b2157
Philip Hurst <youngphil9@gmail.com>
2020-04-29 09:06:55 -0500

    db schema.sql and seeds.sql set

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name)
+VALUES ('Double Cheese Burger');
+
+INSERT INTO burgers (burger_name)
+VALUES ('Bacon Cheese Burger');
+
+INSERT INTO burgers (burger_name)
+VALUES ('Southwestern Burger');
