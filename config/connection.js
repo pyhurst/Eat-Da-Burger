2e945e3edf0785c836748e356bcd3c83aa54801b
Philip Hurst <youngphil9@gmail.com>
2020-04-29 08:55:16 -0500

    set basic mvc with mysql, express, and handlebar boilerplates

---
+++ config/connection.js
@@ -0,0 +1,15 @@
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+
+    port: 3306,
+
+    user: 'root',
+    password: 'root',
+    database: ''
+});
+
+connection.connect(function(err) {
+    if (err) throw err;
+});
---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const connection = require('./config/connection.js');
+
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+app.listen(PORT, function() {
+    console.log('Server listening on: http://localhost:' + PORT);
+});
