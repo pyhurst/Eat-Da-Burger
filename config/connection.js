bf06a4fc6896f24fda8ab18120a5ccd7b3031d14
Philip Hurst <youngphil9@gmail.com>
2020-05-02 11:08:49 -0500

    added heroku jawsDB, format changes

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,10 @@
 const mysql = require('mysql');
 
-const connection = mysql.createConnection({
+const connection;
+if(process.env.JAWSDB_URL){
+    connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+connection = mysql.createConnection({
     host: 'localhost',
 
     port: 3306,
@@ -9,6 +13,7 @@ const connection = mysql.createConnection({
     password: 'root',
     database: 'burgers_db'
 });
+}
 
 connection.connect(function(err) {
     if (err) {
